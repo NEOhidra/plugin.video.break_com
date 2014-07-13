30bb8dd6149a822dc6ae7ce36b1ccfda33e72590
bromix <bromix@gmx.net>
2014-07-13 19:17:48 +0200

    list channels, plot, thumbs and next page

--- default.py
+++ default.py
@@ -1,29 +1,34 @@
 # -*- coding: utf-8 -*-
 
 import os
+import urllib2
+try:
+    from xml.etree import ElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
 
 import bromixbmc
 
 __channel__ = [{'title': 'Featured Videos',
-                'id': ''
+                'id': '-1'
                 },
                {'title': 'Pranks & Fails',
-                'id': ''
+                'id': '534'
                 },
                {'title': 'Trailers',
-                'id': ''
+                'id': '531'
                 },
                {'title': 'Sports',
-                'id': ''
+                'id': '533'
                 },
                {'title': 'Bizarre & Amazing',
-                'id': ''
+                'id': '537'
                 },
                {'title': 'Heartwarming',
-                'id': ''
+                'id': '536'
                 },
                {'title': 'All the animals',
-                'id': ''
+                'id': '535'
                 },
                ]
 
@@ -33,20 +38,108 @@ __channel__ = [{'title': 'Featured Videos',
 __plugin__ = bromixbmc.Plugin()
 
 __FANART__ = os.path.join(__plugin__.getPath(), "fanart.jpg")
+__ICON__ = os.path.join(__plugin__.getPath(), "icon.png")
 __ICON_HIGHLIGHTS__ = os.path.join(__plugin__.getPath(), "resources/media/highlight.png")
 __ICON_SEARCH__ = os.path.join(__plugin__.getPath(), "resources/media/search.png")
 
+__ACTION_SHOW_CHANNEL__ = 'showChannel'
+__ACTION_PLAY__ = 'play'
 __ACTION_SEARCH__ = 'search'
+
+def _getChannelContentXml(id, page):
+    result = None
+    
+    url = 'http://api.break.com'
+    if id=='-1':
+        url+='/invoke/homepage/includeyoutube/'
+    else:
+        url+='/invoke/channel/includeyoutube/'
+        url+=id
+        url+='/'
+        
+    url+=str(page)+'/'
+    url+='25/'
+    
+    if id!='-1':
+        url+='PG/'
+    
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-Agent', 'stagefright/1.2 (Linux;Android 4.4.2)')
+                         ]
+    try:
+        content = opener.open(url)
+        result = ET.XML(content.read())
+    except:
+        # do nothing
+        pass
+    
+    return result
 
 def showIndex():
     for channel in __channel__:
-        __plugin__.addDirectory(channel['title'], fanart=__FANART__)
+        params = {'action': __ACTION_SHOW_CHANNEL__,
+                  'id': channel['id']}
+        __plugin__.addDirectory(channel['title'], params=params, thumbnailImage=__ICON__, fanart=__FANART__)
         pass
     
     params = {'action': __ACTION_SEARCH__}
     __plugin__.addDirectory("[B]"+__plugin__.localize(30000)+"[/B]", params = params, thumbnailImage=__ICON_SEARCH__, fanart=__FANART__)
     
     __plugin__.endOfDirectory()
+    
+def search():
+    success = False
+    
+    keyboard = bromixbmc.Keyboard(__plugin__.localize(30000))
+    if keyboard.doModal():
+        success = True
+        
+        search_string = keyboard.getText().replace(" ", "+")
+        #result = _request('/capi-2.0a/search', params = {'q': search_string})
+        #posts = result.get('posts', [])
+        #_listPosts(posts)
+        
+    __plugin__.endOfDirectory(success)
+    
+def showChannel(id, page):
+    xml = _getChannelContentXml(id, page)
+    if xml:
+        pageCount = 0
+        try:
+            pageCount = int(xml.get('PageCount'))
+        except:
+            pageCount = 1
+            
+        for content in xml:
+            contentName = content.find('ContentTitle').text
+            contentId = content.find('ContentID').text
+            contentPlot = content.find('ContentDescription').text
+            contentThumb = content.find('ThumbNailURL').text
+                 
+            if contentId and contentName:
+                params = {'action': __ACTION_PLAY__,
+                          'id': contentId
+                          }
+                infoLabels = {'plot': contentPlot}
+                __plugin__.addVideoLink(name=contentName, params=params, infoLabels=infoLabels, thumbnailImage=contentThumb, fanart=__FANART__)
+        
+        if page<pageCount:
+            params = {'action': __ACTION_SHOW_CHANNEL__,
+                      'id': id,
+                      'page': str(page+1)
+                      }
+            __plugin__.addDirectory(__plugin__.localize(30001)+' ('+str(page+1)+')', params=params, fanart=__FANART__)
+    
+    __plugin__.endOfDirectory()
 
 
-showIndex()
+action = bromixbmc.getParam('action')
+id = bromixbmc.getParam('id')
+page = int(bromixbmc.getParam('page', '1'))
+
+if action == __ACTION_SHOW_CHANNEL__ and id:
+    showChannel(id, page)
+elif action == __ACTION_SEARCH__:
+    search()
+else:
+    showIndex()
