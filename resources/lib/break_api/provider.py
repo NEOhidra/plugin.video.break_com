3d72392f515d9ada8895ab075ce7283063ab2bda
bromix <bromix@gmx.net>
2014-11-15 00:55:30 +0100

    UPD: kodion tests

--- resources/lib/break_api/provider.py
+++ resources/lib/break_api/provider.py
@@ -1,30 +1,32 @@
 from resources.lib.break_api import Client
-from resources.lib.kodimon import DirectoryItem, VideoItem
-from resources.lib.kodimon.helper import FunctionCache
+
+from resources.lib.kodion.items import DirectoryItem, VideoItem
+from resources.lib.kodion.utils import FunctionCache
+from resources.lib.kodion import constants
 
 __author__ = 'bromix'
 
+from resources.lib import kodion
 
-from resources.lib import kodimon
 
-class Provider(kodimon.AbstractProvider):
+class Provider(kodion.AbstractProvider):
     def __init__(self):
-        kodimon.AbstractProvider.__init__(self)
+        kodion.AbstractProvider.__init__(self)
 
         self._client = Client()
         pass
 
-    def get_fanart(self):
-        return self.create_resource_path('media', 'fanart.jpg')
+    def get_fanart(self, context):
+        return context.create_resource_path('media', 'fanart.jpg')
 
-    def _feed_to_item(self, json_data):
+    def _feed_to_item(self, json_data, context):
         result = []
 
         data = json_data['data']['data']
         for item in data:
             video_item = VideoItem(item['title'],
-                                   self.create_uri(['play', str(item['id'])]))
-            video_item.set_fanart(self.get_fanart())
+                                   context.create_uri(['play', str(item['id'])]))
+            video_item.set_fanart(self.get_fanart(context))
             video_item.set_plot(item['description'])
             image = item.get('thumbnails', [])
             if len(image) > 0:
@@ -36,9 +38,9 @@ class Provider(kodimon.AbstractProvider):
 
         return result
 
-    @kodimon.RegisterPath('^/play/(?P<video_id>.*)/$')
-    def _on_play(self, path, params, re_match):
-        vq = self.get_settings().get_video_quality()
+    @kodion.RegisterProviderPath('^/play/(?P<video_id>.*)/$')
+    def _on_play(self, context, re_match):
+        vq = context.get_settings().get_video_quality()
 
         video_id = re_match.group('video_id')
         video_url = self._client.get_video_url(video_id, vq)
@@ -46,43 +48,47 @@ class Provider(kodimon.AbstractProvider):
                                video_url)
         return video_item
 
-    @kodimon.RegisterPath('^/feed/(?P<feed_id>.*)/$')
-    def _on_feed(self, path, params, re_match):
-        self.set_content_type(kodimon.constants.CONTENT_TYPE_EPISODES)
+    @kodion.RegisterProviderPath('^/feed/(?P<feed_id>.*)/$')
+    def _on_feed(self, context, re_match):
+        params = context.get_params()
+        path = context.get_path()
+
+        context.set_content_type(kodion.constants.content_type.EPISODES)
         result = []
 
         feed_id = int(re_match.group('feed_id'))
         page = int(params.get('page', 1))
 
-        json_data = self.get_function_cache().get(FunctionCache.ONE_MINUTE, self._client.get_feed, feed_id, page)
-        result.extend(self._feed_to_item(json_data))
+        json_data = context.get_function_cache().get(FunctionCache.ONE_MINUTE, self._client.get_feed, feed_id, page)
+        result.extend(self._feed_to_item(json_data, context))
 
         # test next page
-        next_page = page+1
-        json_data = self.get_function_cache().get(FunctionCache.ONE_MINUTE, self._client.get_feed, feed_id, next_page)
-        next_page_result = self._feed_to_item(json_data)
+        next_page = page + 1
+        json_data = context.get_function_cache().get(FunctionCache.ONE_MINUTE, self._client.get_feed, feed_id, next_page)
+        next_page_result = self._feed_to_item(json_data, context)
         if len(next_page_result) > 0:
-            next_page_item = self.create_next_page_item(page, path, params)
+            next_page_item = kodion.items.create_next_page_item(context, page, path, params)
             result.append(next_page_item)
             pass
 
         return result
 
-    def on_root(self, path, params, re_match):
+    def on_root(self, context, re_match):
         result = []
 
-        json_data = self.get_function_cache().get(FunctionCache.ONE_DAY, self._client.get_home)
+        json_data = context.get_function_cache().get(FunctionCache.ONE_DAY, self._client.get_home)
         collection = json_data['data']['data']['collection']
         for item in collection:
             title = item['name']
             if title != u'Galleries':
                 item_id = str(item['id'])
                 feed_item = DirectoryItem(title,
-                                          self.create_uri(['feed', item_id]))
-                feed_item.set_fanart(self.get_fanart())
+                                          context.create_uri(['feed', item_id]))
+                feed_item.set_fanart(self.get_fanart(context))
                 result.append(feed_item)
                 pass
             pass
 
         return result
+
     pass
